b0c09ae4f433b9bad90201913157b3eb026f0d4a
Beki-050701 <107194252+Beki-050701@users.noreply.github.com>
2023-10-03 04:06:00 +0200

    1st commit - new site

---
+++ main.js
@@ -0,0 +1,288 @@
+function hideParagraph() {
+    var paragraphs = document.getElementsByClassName("myParagraph");
+    for(var i=0; i<paragraphs.length; i++) {
+        paragraphs[i].style.visibility = "hidden";
+    }
+}
+setTimeout(hideParagraph, 236000);
+
+//para1
+function changeParagraph1() {
+    var paragraphs = document.getElementsByClassName("myParagraph1");
+    for(var i=0; i<paragraphs.length; i++) {
+        paragraphs[i].style.visibility = "visible";
+    }
+}
+setTimeout(changeParagraph1, 237000);
+
+function hideParagraph1() {
+    var paragraphs = document.getElementsByClassName("myParagraph1");
+    for(var i=0; i<paragraphs.length; i++) {
+        paragraphs[i].style.visibility = "hidden";
+    }
+}
+setTimeout(hideParagraph1, 262000);
+
+//para2
+function changeParagraph2() {
+    var paragraphs = document.getElementsByClassName("myParagraph2");
+    for(var i=0; i<paragraphs.length; i++) {
+        paragraphs[i].style.visibility = "visible";
+    }
+}
+setTimeout(changeParagraph2, 263000);
+
+function hideParagraph2() {
+    var paragraphs = document.getElementsByClassName("myParagraph2");
+    for(var i=0; i<paragraphs.length; i++) {
+        paragraphs[i].style.visibility = "hidden";
+    }
+}
+setTimeout(hideParagraph2, 288000);
+
+//para3
+function changeParagraph3() {
+    var paragraphs = document.getElementsByClassName("myParagraph3");
+    for(var i=0; i<paragraphs.length; i++) {
+        paragraphs[i].style.visibility = "visible";
+    }
+}
+setTimeout(changeParagraph3, 289000);
+
+function hideParagraph3() {
+    var paragraphs = document.getElementsByClassName("myParagraph3");
+    for(var i=0; i<paragraphs.length; i++) {
+        paragraphs[i].style.visibility = "hidden";
+    }
+}
+setTimeout(hideParagraph3, 314000);
+
+//para4
+function changeParagraph4() {
+    var paragraphs = document.getElementsByClassName("myParagraph4");
+    for(var i=0; i<paragraphs.length; i++) {
+        paragraphs[i].style.visibility = "visible";
+    }
+}
+setTimeout(changeParagraph4, 315000);
+
+function hideParagraph4() {
+    var paragraphs = document.getElementsByClassName("myParagraph4");
+    for(var i=0; i<paragraphs.length; i++) {
+        paragraphs[i].style.visibility = "hidden";
+    }
+}
+setTimeout(hideParagraph4, 337000);
+
+//para5
+function changeParagraph5() {
+    var paragraphs = document.getElementsByClassName("myParagraph5");
+    for(var i=0; i<paragraphs.length; i++) {
+        paragraphs[i].style.visibility = "visible";
+    }
+}
+setTimeout(changeParagraph5, 338000);
+
+function hideParagraph5() {
+    var paragraphs = document.getElementsByClassName("myParagraph5");
+    for(var i=0; i<paragraphs.length; i++) {
+        paragraphs[i].style.visibility = "hidden";
+    }
+}
+setTimeout(hideParagraph5, 363000);
+
+//para6
+function changeParagraph6() {
+    var paragraphs = document.getElementsByClassName("myParagraph6");
+    for(var i=0; i<paragraphs.length; i++) {
+        paragraphs[i].style.visibility = "visible";
+    }
+}
+setTimeout(changeParagraph6, 364000);
+
+function hideParagraph6() {
+    var paragraphs = document.getElementsByClassName("myParagraph6");
+    for(var i=0; i<paragraphs.length; i++) {
+        paragraphs[i].style.visibility = "hidden";
+    }
+}
+setTimeout(hideParagraph6, 389000);
+
+//para7
+function changeParagraph7() {
+    var paragraphs = document.getElementsByClassName("myParagraph7");
+    for(var i=0; i<paragraphs.length; i++) {
+        paragraphs[i].style.visibility = "visible";
+    }
+}
+setTimeout(changeParagraph7, 390000);
+
+function hideParagraph7() {
+    var paragraphs = document.getElementsByClassName("myParagraph7");
+    for(var i=0; i<paragraphs.length; i++) {
+        paragraphs[i].style.visibility = "hidden";
+    }
+}
+setTimeout(hideParagraph7, 420000);
+
+//para8
+function changeParagraph8() {
+    var paragraphs = document.getElementsByClassName("myParagraph8");
+    for(var i=0; i<paragraphs.length; i++) {
+        paragraphs[i].style.visibility = "visible";
+    }
+}
+setTimeout(changeParagraph8, 421000);
+
+function hideParagraph8() {
+    var paragraphs = document.getElementsByClassName("myParagraph8");
+    for(var i=0; i<paragraphs.length; i++) {
+        paragraphs[i].style.visibility = "hidden";
+    }
+}
+setTimeout(hideParagraph8, 446000);
+
+//para9
+function changeParagraph9() {
+    var paragraphs = document.getElementsByClassName("myParagraph9");
+    for(var i=0; i<paragraphs.length; i++) {
+        paragraphs[i].style.visibility = "visible";
+    }
+}
+setTimeout(changeParagraph9, 447000);
+
+function hideParagraph9() {
+    var paragraphs = document.getElementsByClassName("myParagraph9");
+    for(var i=0; i<paragraphs.length; i++) {
+        paragraphs[i].style.visibility = "hidden";
+    }
+}
+setTimeout(hideParagraph9, 477000);
+
+//para10
+function changeParagraph10() {
+    var paragraphs = document.getElementsByClassName("myParagraph10");
+    for(var i=0; i<paragraphs.length; i++) {
+        paragraphs[i].style.visibility = "visible";
+    }
+}
+setTimeout(changeParagraph10, 478000);
+
+function hideParagraph10() {
+    var paragraphs = document.getElementsByClassName("myParagraph10");
+    for(var i=0; i<paragraphs.length; i++) {
+        paragraphs[i].style.visibility = "hidden";
+    }
+}
+setTimeout(hideParagraph10, 503000);
+
+//para11
+function changeParagraph11() {
+    var paragraphs = document.getElementsByClassName("myParagraph11");
+    for(var i=0; i<paragraphs.length; i++) {
+        paragraphs[i].style.visibility = "visible";
+    }
+}
+setTimeout(changeParagraph11, 504000);
+
+function hideParagraph11() {
+    var paragraphs = document.getElementsByClassName("myParagraph11");
+    for(var i=0; i<paragraphs.length; i++) {
+        paragraphs[i].style.visibility = "hidden";
+    }
+}
+setTimeout(hideParagraph11, 529000);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function hidediv() {
+    var elements = document.getElementsByClassName("block");
+    for(var i=0; i<elements.length; i++) {
+        elements[i].style.visibility = "hidden";
+    }
+}
+setTimeout(hidediv, 42000);
+
+// block1 div
+function showdiv1() {
+    var elements = document.getElementsByClassName("block1");
+        for(var i=0; i<elements.length; i++) {
+        elements[i].style.visibility = "visible";
+    }
+}
+setTimeout(showdiv1, 43000);
+
+function hidediv1() {
+    var elements = document.getElementsByClassName("block1");
+    for(var i=0; i<elements.length; i++) {
+        elements[i].style.visibility = "hidden";
+    }
+}
+setTimeout(hidediv1, 88000);
+
+// block2 div
+function showdiv2() {
+    var elements = document.getElementsByClassName("block2");
+        for(var i=0; i<elements.length; i++) {
+        elements[i].style.visibility = "visible";
+    }
+}
+setTimeout(showdiv2, 89000);
+
+function hidediv2() {
+    var elements = document.getElementsByClassName("block2");
+    for(var i=0; i<elements.length; i++) {
+        elements[i].style.visibility = "hidden";
+    }
+}
+setTimeout(hidediv2, 134000);
+
+// block3 div
+function showdiv3() {
+    var elements = document.getElementsByClassName("block3");
+        for(var i=0; i<elements.length; i++) {
+        elements[i].style.visibility = "visible";
+    }
+}
+setTimeout(showdiv3, 135000);
+
+function hidediv3() {
+    var elements = document.getElementsByClassName("block3");
+    for(var i=0; i<elements.length; i++) {
+        elements[i].style.visibility = "hidden";
+    }
+}
+setTimeout(hidediv3, 160000);
+
+// block4 div
+function showdiv4() {
+    var elements = document.getElementsByClassName("block4");
+        for(var i=0; i<elements.length; i++) {
+        elements[i].style.visibility = "visible";
+    }
+}
+setTimeout(showdiv4, 161000);
+
+function hidediv4() {
+    var elements = document.getElementsByClassName("block4");
+    for(var i=0; i<elements.length; i++) {
+        elements[i].style.visibility = "hidden";
+    }
+}
+setTimeout(hidediv4, 201000);
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function showVideo(){
+    var elements = document.getElementsByClassName("movecursor");
+        for(var i=0; i<elements.length; i++) {
+        elements[i].style.visibility = "visible";
+    }
+}
+setTimeout(showVideo, 202000);
+
+function hideVideo(){
+    var elements = document.getElementsByClassName("movecursor");
+    for(var i=0; i<elements.length; i++) {
+        elements[i].style.visibility = "hidden";
+    }
+}
+setTimeout(hideVideo, 206000);
